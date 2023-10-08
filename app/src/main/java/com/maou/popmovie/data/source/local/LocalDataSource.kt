56fc097e560278ff13b5b10d958c953c0cc334df
shaqna <shaquillerizkirn@gmail.com>
2023-10-08 17:03:05 +0700

    create data mapper

--- app/src/main/java/com/maou/popmovie/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/maou/popmovie/data/source/local/LocalDataSource.kt
@@ -1,9 +1,13 @@
 package com.maou.popmovie.data.source.local
 
 import com.maou.popmovie.data.source.local.dao.MovieDao
+import com.maou.popmovie.data.source.local.entity.MovieEntity
+import kotlinx.coroutines.flow.Flow
 
 class LocalDataSource(
     private val movieDao: MovieDao
 ) {
+    suspend fun insertMovies(movies: List<MovieEntity>) = movieDao.insertMovieList(movies)
 
+    fun selectAllMovie(): Flow<List<MovieEntity>> = movieDao.selectAllMovie()
 }
