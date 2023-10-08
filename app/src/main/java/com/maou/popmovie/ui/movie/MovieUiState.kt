3dea9944bfdd28c38fdec4d7ef3146c82766741c
shaqna <shaquillerizkirn@gmail.com>
2023-10-08 20:35:11 +0700

    create movie ui state

---
+++ app/src/main/java/com/maou/popmovie/ui/movie/MovieUiState.kt
@@ -0,0 +1,10 @@
+package com.maou.popmovie.ui.movie
+
+import com.maou.popmovie.domain.model.Movie
+
+sealed class MovieUiState {
+    data object Init: MovieUiState()
+    data class Loading(val isLoading: Boolean): MovieUiState()
+    data class Error(val message: Boolean): MovieUiState()
+    data class Success(val movies: List<Movie>): MovieUiState()
+}
