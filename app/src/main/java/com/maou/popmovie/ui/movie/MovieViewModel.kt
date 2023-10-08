d48a23d1b03b96d002355545f625e77a25b2a7dd
shaqna <shaquillerizkirn@gmail.com>
2023-10-08 21:23:06 +0700

    complete viewModel

--- app/src/main/java/com/maou/popmovie/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/maou/popmovie/ui/movie/MovieViewModel.kt
@@ -1,11 +1,41 @@
 package com.maou.popmovie.ui.movie
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.maou.popmovie.domain.usecase.MovieUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 class MovieViewModel(
     private val movieUseCase: MovieUseCase
 ): ViewModel() {
 
+    private val _movies: MutableStateFlow<MovieUiState> = MutableStateFlow(MovieUiState.Init)
+    private val movies: StateFlow<MovieUiState> = _movies
+
+    fun fetchMovies() {
+        viewModelScope.launch {
+            movieUseCase.getAllMovies()
+                .onStart {
+                    _movies.value = MovieUiState.Loading(true)
+                }
+                .catch { e ->
+                    _movies.value = MovieUiState.Loading(false)
+                    _movies.value = MovieUiState.Error(e.message.toString())
+                }
+                .collect { result ->
+                    _movies.value = MovieUiState.Loading(false)
+                    result.fold(onSuccess = { movies ->
+                        _movies.value = MovieUiState.Success(movies)
+                    }, onFailure = {err ->
+                        _movies.value = MovieUiState.Error(err.message.toString())
+                    })
+                }
+        }
+    }
 
 }
