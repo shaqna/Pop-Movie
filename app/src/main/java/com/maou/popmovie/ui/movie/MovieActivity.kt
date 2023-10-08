66c02775079e5c28c3a95c37677ecb82ac2d9daf
shaqna <shaquillerizkirn@gmail.com>
2023-10-08 20:28:54 +0700

    add movie activity and viewmodel

---
+++ app/src/main/java/com/maou/popmovie/ui/movie/MovieActivity.kt
@@ -0,0 +1,12 @@
+package com.maou.popmovie.ui.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maou.popmovie.R
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
---
+++ app/src/main/java/com/maou/popmovie/ui/movie/MovieViewModel.kt
@@ -0,0 +1,11 @@
+package com.maou.popmovie.ui.movie
+
+import androidx.lifecycle.ViewModel
+import com.maou.popmovie.domain.usecase.MovieUseCase
+
+class MovieViewModel(
+    private val movieUseCase: MovieUseCase
+): ViewModel() {
+
+
+}
