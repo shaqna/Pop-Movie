1b8226f568690fd83df949074e46178feb62d0f5
shaqna <shaquillerizkirn@gmail.com>
2023-10-08 17:01:22 +0700

    completed repository implemetation: PopMovieRepositoryImpl

--- app/src/main/java/com/maou/popmovie/data/repository/PopMovieRepositoryImpl.kt
+++ app/src/main/java/com/maou/popmovie/data/repository/PopMovieRepositoryImpl.kt
@@ -1,18 +1,32 @@
 package com.maou.popmovie.data.repository
 
+import com.maou.popmovie.data.mapper.toListModel
 import com.maou.popmovie.data.source.local.LocalDataSource
 import com.maou.popmovie.data.source.local.dao.MovieDao
 import com.maou.popmovie.data.source.remote.service.ApiService
 import com.maou.popmovie.domain.model.Movie
 import com.maou.popmovie.domain.repository.PopMovieRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 
 class PopMovieRepositoryImpl(
-    private val apiService: ApiService,
     private val localSource:  LocalDataSource
-
 ): PopMovieRepository {
-    override fun getPopMovie(apiKey: String): Flow<List<Movie>> {
-        TODO("Not yet implemented")
+    override fun getPopMovie(apiKey: String): Flow<Result<List<Movie>>> {
+        return flow {
+            try {
+                val result = localSource.selectAllMovie()
+                result.collect {listMovieEntity ->
+                    emit(Result.success(listMovieEntity.toListModel()))
+                }
+            } catch (e: Exception) {
+                emit(Result.failure(e))
+            }
+
+        }.catch { e->
+            emit(Result.failure(e))
+        }
     }
 }
