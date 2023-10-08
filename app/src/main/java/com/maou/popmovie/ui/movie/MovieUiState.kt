33595201155f1bede39e5a5b00053539652b2599
shaqna <shaquillerizkirn@gmail.com>
2023-10-08 20:46:03 +0700

    fixed variable type

--- app/src/main/java/com/maou/popmovie/ui/movie/MovieUiState.kt
+++ app/src/main/java/com/maou/popmovie/ui/movie/MovieUiState.kt
@@ -5,6 +5,6 @@ import com.maou.popmovie.domain.model.Movie
 sealed class MovieUiState {
     data object Init: MovieUiState()
     data class Loading(val isLoading: Boolean): MovieUiState()
-    data class Error(val message: Boolean): MovieUiState()
+    data class Error(val message: String): MovieUiState()
     data class Success(val movies: List<Movie>): MovieUiState()
 }
